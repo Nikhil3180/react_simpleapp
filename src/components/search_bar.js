ca1a54046bb74ee99bddefdc1ab4e1ec2a78953c
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-06-14 16:22:36 +0530

    state implemented

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,27 @@
 import React , {Component} from 'react';
 
 class SearchBar extends Component {
-  render() {
-      return <input />;
+
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
   }
-}
+
+  render() {
+      return (
+        <div>
+      <input onChange = {event => this.setState({term : event.target.value})} />
+      <br/>
+      Value of the input : {this.state.term}
+      </div>
+      );
+  }
+
+//   onInputChange(event) {
+//    this.setState({term : event.target.value});
+// console.log(event.target.value);
+//   }
+
+ }
 
 export default SearchBar;
