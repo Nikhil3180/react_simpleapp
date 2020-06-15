1fe7f06b1bc48e063776e7be1583f46a82ad8e10
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-06-15 22:41:08 +0530

    functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -12,10 +12,33 @@ YTSearch({key : API_KEY, term : 'surfboards'}, function(data){
 
 // create a new component. This component produce some HTML
 
-const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>);
+// const App = () => {
+//   return (<div>
+//     <SearchBar />
+//   </div>);
+// }
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key : API_KEY, term : 'surfboards'}, (data) => {
+      this.setState({videos : data});
+      console.log(data)
+    });
+
+  }
+
+  render() {
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
+  }
 }
 
 // Take this components's generated HTML and put it on the page (in the DOM)
