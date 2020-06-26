1048755fb647e873dbd08e6422f62d62ebee1d4b
Nikhil Goel <nikhilgoel@Nikhils-MacBook-Pro.local>
2020-06-26 15:57:51 +0530

    search logic implemented

--- src/index.js
+++ src/index.js
@@ -32,20 +32,22 @@ class App extends Component {
       videos: [],
     selectedVideo: null
     };
-
-    YTSearch({key : API_KEY, term : 'asal mei'}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
-
+this.videoSearch('Iron Man')
   }
+
+videoSearch(term){
+  YTSearch({key : API_KEY, term :term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  });
+}
 
   render() {
     return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
       onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
